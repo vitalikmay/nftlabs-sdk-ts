b6e6b1fd5d70dccb7d31db11a9e8541fe35ca0e9
ayush <62694274+ayshptk@users.noreply.github.com>
2021-12-29 00:27:10 +0530

    Create volume.test.ts

---
+++ test/volume.test.ts
@@ -0,0 +1,61 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { BundleDropModule } from "../src/index";
+import { appModule, sdk, signers } from "./before.test";
+import { ethers } from "hardhat";
+import { expect, assert } from "chai";
+
+global.fetch = require("node-fetch");
+
+describe("Bundle Module (aka Collection Module)", async () => {
+  let bundleDropModule: BundleDropModule;
+
+  let adminWallet: SignerWithAddress;
+  let testSigners: any[];
+  before(() => {
+    [adminWallet] = signers;
+    testSigners = [];
+    console.time("wallet");
+    for (let i = 0; i < 100; i++) {
+      testSigners.push(new ethers.Wallet.createRandom());
+      console.log(`${testSigners[i].address} was created`);
+    }
+    console.log("===WALLETS CREATED===");
+    console.timeEnd("wallet");
+  });
+
+  it("should do something", async () => {
+    console.log("testSigners", testSigners.length);
+    const allowList = [];
+    testSigners.forEach((signer) => {
+      try {
+        allowList.push(signer.address);
+      } catch (e) {
+        console.log(signer);
+      }
+    });
+    bundleDropModule = await appModule.deployBundleDropModule({
+      name: "test",
+      description: "test",
+      primarySaleRecipientAddress: "0x0000000000000000000000000000000000000000",
+    });
+    await bundleDropModule.lazyMintBatch([{ name: "test" }]);
+
+    console.log("bundleDropModule", bundleDropModule.address);
+
+    const factory = bundleDropModule.getClaimConditionFactory();
+    const claimPhase = factory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 15000,
+      maxQuantityPerTransaction: 1,
+    });
+    await claimPhase.setSnapshot(allowList);
+
+    await bundleDropModule.setClaimCondition("0", factory);
+
+    for (let i = 0; i < testSigners.length; i++) {
+      sdk.setProviderOrSigner(testSigners[i]);
+      await bundleDropModule.claim(0, 1);
+      console.log("claimed successfully for ", i);
+    }
+  });
+});
